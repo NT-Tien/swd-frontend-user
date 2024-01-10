5cb29de4351831d2c9d362df536709ef69893032
HaiDank <132982748+HaiDank@users.noreply.github.com>
2024-01-10 21:24:59 +0700

    Setup environment

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx}",
+		'./src/**/**/*.{html,js,jsx}',
+		'./src/**/**/**/*.{html,js,jsx}',
+		'./src/*.{html,js,jsx}',
+		'./index.html',
+  ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
