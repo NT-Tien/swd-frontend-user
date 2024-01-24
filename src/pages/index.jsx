21dd3b70e4977def831c1da9651a66f3eb60a115
HaiDank <132982748+HaiDank@users.noreply.github.com>
2024-01-24 16:16:53 +0700

    asd

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,8 @@
 export {default as NotFound}  from './NotFound';
 export {default as ShoppingCartPage} from './cartPage/ShoppingCartPage';
 export {default as ShopPage} from './shopPage/ShopPage';
-export {default as HomePage} from './homePage/HomePage';
 export {default as AboutUsPage } from './aboutUsPage/AboutUsPage';
 export {default as ContactPage} from './contactPage/ContactPage';
 export {default as BookingPage} from './bookingPage/BookingPage';
 export {default as UserLayout} from './UserLayout'
+export {default as HomePage} from './homePage/HomePage'
